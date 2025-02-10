9df60f44e21ae8ed25acd4c9e85aa186e214ff44
ak-fe-rottengrapes <ak.rottengrapes@gmail.com>
2025-02-10 16:15:48 +0530

    GDV

--- src/middleware.js
+++ src/middleware.js
@@ -1,16 +1,23 @@
 import { NextResponse } from 'next/server'
 import { getToken } from 'next-auth/jwt'
-
 export async function middleware(req) {
     const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
+    if (req.nextUrl.pathname.startsWith('/admin') && token.is_admin === false) {
+        return NextResponse.redirect(new URL('/unauthorized', req.url))
+    }
+
+    if (req.nextUrl.pathname.startsWith('/employee') && token.is_employee === false) {
+        return NextResponse.redirect(new URL('/unauthorized', req.url))
+    }
 
     if (!token) {
         return NextResponse.redirect(new URL('/auth/login', req.url))
     }
+    
 
     return NextResponse.next()
 }
 
 export const config = {
-    matcher: ['/admin', '/employee','/user'], 
+    matcher: ['/admin/:path*', '/employee','/user'], 
 }
